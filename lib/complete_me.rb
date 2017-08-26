0c67078759a528a145aacc92a148ee2f97aabd71
Josi <junipertmcclellan@gmail.com>
2017-08-26 17:47:39 -0600

    debugged suggest; made spec harness work

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -22,13 +22,19 @@ class CompleteMe
     working.end_status = true
   end
 
+  def populate(words)
+    words.each_line do |line|
+      insert line.chomp
+    end
+  end
+
   def select(prefix, word)
     @selections[prefix][word] += 1
   end
 
-  def suggest(prefix, start)
-    unordered_suggestions = generate_suggestions
-    order_suggestions(unordered_suggestions)
+  def suggest(prefix)
+    unordered_suggestions = generate_suggestions(prefix)
+    order_suggestions(prefix, unordered_suggestions)
   end
 
   def find_start_node(prefix)
@@ -40,27 +46,28 @@ class CompleteMe
     current_node
   end
 
-  def generate_suggestions(prefix, start_node)
+  def generate_suggestions(prefix)
     start_node = find_start_node(prefix)
     incompletes = []
-    complete = []
-    incompletes << [prefix, start]
+    completes = []
+    incompletes << [prefix, start_node]
     until incompletes.empty?
        word_so_far, working = incompletes.pop
-       complete << word_so_far if working.end?
+       completes << word_so_far if working.end?
 
-       working.each_pair do |character, child|
+       working.children.each_pair do |character, child|
          new_word_so_far = word_so_far + character
          incompletes << [new_word_so_far, child]
        end
      end
+     completes
   end
 
   def order_suggestions(prefix, suggestions)
-    selections_from_prefix = @selections[prefix]
+    selection_counts_from_prefix = @selections[prefix]
     suggestions.sort do |a_suggestion, b_suggestion|
-      a_count = selection_counts_from_prefix[a_suggestions]
-      b_count = selection_counts_from_prefix[b_suggestions]
+      a_count = selection_counts_from_prefix[a_suggestion]
+      b_count = selection_counts_from_prefix[b_suggestion]
       b_count <=> a_count
     end
   end

3022c747d1d7298d0fc5d1f5cdec1f15cf97494d
Josi <junipertmcclellan@gmail.com>
2017-08-29 12:32:19 -0600

    add tests for delete

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -8,9 +8,10 @@ require "./lib/complete_me"
 
 
 class CompleteMeTest < Minitest::Test
-  attr_reader :cm
+  attr_reader :cm, :root
   def setup
     @cm = CompleteMe.new
+    @root = cm.root
   end
 
   def test_starting_count
@@ -25,14 +26,14 @@ class CompleteMeTest < Minitest::Test
   def test_insert_creates_a_node_for_each_letter_of_a_word
     cm.insert('me')
 
-    assert_instance_of Node, cm.root.children['m']
-    assert_instance_of Node, cm.root.children['m'].children['e']
+    assert_instance_of Node, root.children['m']
+    assert_instance_of Node, root.children['m'].children['e']
   end
 
   def test_insert_indicates_an_end_of_a_word_at_a_words_last_letter_node
     cm.insert('me')
 
-    assert true, cm.root.children['m'].end?
+    assert true, root.children['m'].end?
   end
 
   def test_insert_inserts_single_word
@@ -88,7 +89,7 @@ class CompleteMeTest < Minitest::Test
   def test_find_node_returns_node_of_last_character_of_given_prefix
     cm.insert('meal')
 
-    assert_equal cm.root.children['m'].children['e'], cm.find_node('me')
+    assert_equal root.children['m'].children['e'], cm.find_node('me')
   end
 
   def test_find_node_returns_nil_if_prefix_path_doesnt_exist
@@ -109,6 +110,97 @@ class CompleteMeTest < Minitest::Test
     assert_equal result, cm.order_suggestions("piz", piz_suggestions)
   end
 
+  def test_delete_removes_end_status_of_node_with_children
+    insert_words(["A", "AAA"])
+    cm.delete("A")
+
+    deleted_word_node = root.children["A"]
+    descendent_word_node = deleted_word_node.children["A"].children["A"]
+
+    refute deleted_word_node.end?
+    assert_instance_of Node, descendent_word_node
+
+    assert_equal 1, cm.count
+  end
+
+  def test_delete_removes_unnecessary_nodes_back_to_ending_ancestor
+    insert_words(["A", "AAA"])
+    cm.delete("AAA")
+
+    ending_ancestor = root.children["A"]
+
+    assert_instance_of Node, ending_ancestor
+    assert ending_ancestor.end?
+    assert_nil ending_ancestor.children["A"]
+
+    assert_equal 1, cm.count
+  end
+
+  def test_delete_removes_unnecessary_nodes_back_to_branching_node
+    insert_words(["AAA", "ABB"])
+    cm.delete("ABB")
+
+    branching_ancestor = root.children['A']
+    remaining_cousin = branching_ancestor.children['A'].children['A']
+
+    assert_nil branching_ancestor.children["B"]
+    assert_instance_of Node, remaining_cousin
+
+    assert_equal 1, cm.count
+  end
+
+  def test_delete_removes_word_cousined_only_by_root
+    insert_words(["AA", "BB"])
+    cm.delete("BB")
+
+    remaining_cousin = root.children["A"].children["A"]
+
+    assert_nil root.children["B"]
+    assert_instance_of Node, remaining_cousin
+
+    assert_equal 1, cm.count
+  end
+
+  def test_delete_removes_last_word
+    cm.insert("AA")
+    cm.delete("AA")
+
+    assert_nil root.children["A"]
+
+    assert_equal 0, cm.count
+  end
+
+  def test_delete_does_nothing_if_word_is_not_present
+    cm.insert('AA')
+
+    cm.delete('AAA')
+    cm.delete('A')
+    cm.delete('B')
+    cm.delete('')
+
+    assert_instance_of Node, root.children['A'].children['A']
+
+    assert_equal 1, cm.count
+  end
+
+  def test_delete_returns_true_if_word_was_deleted
+    insert_words(["pizza", "pardva", "pardvark", "pard", "foo"])
+    assert @cm.delete('pard')
+    assert @cm.delete('pardvark')
+    assert @cm.delete('pardva')
+    assert @cm.delete('pizza')
+    assert @cm.delete('foo')
+  end
+
+  def test_delete_returns_false_if_no_word_was_deleted
+    cm.insert('AA')
+
+    refute cm.delete('AAA')
+    refute cm.delete('A')
+    refute cm.delete('B')
+    refute cm.delete('')
+  end
+
   def test_populate_from_csv_inserts_300_denver_addresses
     cm.populate_from_csv('./test/data/addresses_first_300.csv')
 
@@ -116,6 +208,7 @@ class CompleteMeTest < Minitest::Test
   end
 
   def test_populate_from_csv_inserts_306009_denver_addresses
+    skip
     cm.populate_from_csv('./test/data/addresses.csv')
 
     assert_equal 306009, cm.count
